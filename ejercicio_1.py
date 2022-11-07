894445431b59fb641dcda652137147ea2a64ee3e
lala1981 <116035357+lala1981@users.noreply.github.com>
2022-11-07 17:02:23 -0300

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,41 @@
+# Bucles [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con listas
+
+# Objetivo:
+# Armar una lista de contenga los temas
+# que ha vistoen este curso.
+# Podrá armar la lista inicialzandola
+# con los valores solicitados
+# o empezando con una lista vacia y agregar
+# cada valor con append
+
+# Alumno:
+# Crear una variable del tipo lista
+# llamada contenidos_curso
+# En dicha variable deberá almacenar los siguientes
+# elementos tipo texto en el siguiente orden:
+
+# python
+# variables
+# condicionales
+# listas
+# bucles
+
+
+# Imprimir en pantalla la variable contenidos_curso
+
+contenidos_curso = []
+contenidos_curso.append ('python')
+contenidos_curso.append ('variables')
+contenidos_curso.append ('condicionales')
+contenidos_curso.append ('listas')
+contenidos_curso.append ('bucles')
+print(contenidos_curso)
